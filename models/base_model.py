7910de6214bc0fa4cec0d07232b1b5d18ac0f9c2
amch-med23 <skyied.med2023@gmail.com>
2023-12-19 19:39:27 +0100

    Task 6, 7, 8, 9, 10: adding the required new files (db_storage), and updating the existing files to accomply with the tasks requirements

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,19 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
+from models import storage_type
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
     
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
     
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -23,14 +25,18 @@ class BaseModel:
             self.updated_at = datetime.now()
         
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-            self.__dict__.update(kwargs)
+            for k in kwargs:
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
+                elif k != '__class__':
+                    setattr(self, k, kwargs[k])
+            if storage_type == "db":
+                if not hasattr(kwargs, 'id'):
+                    setattr(self, 'db', str(uuid.uuid4()))
+                if not hasattr(kwargs, 'created_at'):
+                    setattr(self, 'created_at', datetime.now())
+                if not hasattr(kwargs, 'updated_at'):
+                    setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -58,7 +64,9 @@ class BaseModel:
         if dictionary.get('_sa_instance_state'):
             dictinary.pop('_sa_instance_state')
 
-    def delete(self):
-        storage.delete(self)
-            
         return dictionary
+
+    def delete(self):
+        """ delets the current instance from the storage """
+        from models import storage
+        storage.delete(self)
