18c6ce69e3e2b903a496e3e2a1800dac1bb98a97
zaynOm <ouaziz0626@gmail.com>
2023-12-20 12:00:49 +0100

    task 6: adding DbStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-storage = FileStorage()
+if HBNB_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DbStorage
+    storage = DbStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
