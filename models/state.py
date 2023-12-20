2d3e68fc20fbc1be6e4a6a1dd162baae5125e213
zaynOm <ouaziz0626@gmail.com>
2023-12-20 11:55:33 +0100

    task 6: completing the task and fixing errors

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete, delete-orphan")
+
+    @property
+    def cities(self):
+        cities_in_state = []
+        all_cities = storage.all(City)
+        for _, v in all_cities.items():
+            if v.state_id == self.id:
+              cities_in_state.append(v)
+        return cities_in_state
