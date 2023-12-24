428ad610981ca033aeff7943c386cbfebeb8b29b
zaynOm <ouaziz0626@gmail.com>
2023-12-24 22:45:41 +0100

    adding ORM functionality

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    ""
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            'Place', secondary='place_amenity', back_populates='amenities'
+        )
+    else:
+        name = ''
