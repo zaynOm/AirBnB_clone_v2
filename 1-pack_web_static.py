0b35eb0eeeb2ffec9af143c4c730975a02417cef
zaynOm <ouaziz0626@gmail.com>
2024-01-12 00:32:03 +0100

    task 1: Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"do_pack module"
+from fabric.operations import local
+from datetime import datetime
+
+
+def do_pack():
+    "Compress web_static"
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    folder = "/versions"
+    mkdir = "mkdir -p " + folder
+    path = f"{folder}/web_static_{date}.tgz"
+    if local(f"{mkdir} && tar -cvzf {path} web_static").failed:
+        return None
+    return path
