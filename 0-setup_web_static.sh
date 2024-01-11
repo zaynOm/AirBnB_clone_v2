54a32c21b400f1553ac36ca354c163f0c4db16d0
zaynOm <ouaziz0626@gmail.com>
2024-01-11 22:52:42 +0100

    task 0: Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,24 +8,24 @@ echo "Hello from School!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 echo "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	add_header Y-Served-By $HOSTNAME;
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        add_header Y-Served-By $HOSTNAME;
 
-	location /hbnb_static {
-		alias /data/web_static/current/;
-		try_files \$uri \$uri/ @404;
-	}
+        location /hbnb_static {
+                alias /data/web_static/current/;
+                try_files \$uri \$uri/ @404;
+        }
 
-	location /redirect_me {
-		return 301 /;
-	}
+        location /redirect_me {
+                return 301 /;
+        }
 
-	location @404 {
-		return 404 'Ceci n'est pas une page';
-	}
+        location @404 {
+                return 404 \"Ceci n'est pas une page\";
+        }
 }" > /etc/nginx/sites-available/default
 
 service nginx restart
